4ce74d1ffb99c1988a696fceb0ec69e6b79166d8
Henrik Kühl <henrikkyhl@hotmail.com>
2019-10-23 15:48:25 +0200

    DataTableWithMultipleObjects example has been added

---
+++ CalculatorLib.SpecFlowTests/DataTableWithMultipleObjects.feature
@@ -0,0 +1,12 @@
+﻿Feature: DataTableWithMultipleObjects
+    In order to avoid silly mistakes
+    As a math idiot
+    I want to be told the age of a person with a given birthdate
+
+Scenario: Calculate age of first person
+    Given I have entered the following list of person data
+    | Name | BirthDate  |
+    | Ann  | 1998-06-30 |
+    | Joe  | 2001-06-30 |
+    When I press Calculate age of the first person
+    Then The result should be 21 years for the first person  
---
+++ CalculatorLib.SpecFlowTests/DataTableWithMultipleObjectsSteps.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using Xunit;
+
+namespace CalculatorLib.SpecFlowTests
+{
+    [Binding]
+    public class DataTableWithMultipleObjectsSteps
+    {
+        List<Person> persons;
+        int age;
+        Calculator calculator = new Calculator();
+
+        [Given(@"I have entered the following list of person data")]
+        public void GivenIHaveEnteredTheFollowingListOfPersonData(Table table)
+        {
+            persons = table.CreateSet<Person>() as List<Person>;
+        }
+
+        [When(@"I press Calculate age of the first person")]
+        public void WhenIPressCalculateAgeOfTheFirstPerson()
+        {
+            age = calculator.CalculateAge(persons[0]);
+        }
+
+        [Then(@"The result should be (.*) years for the first person")]
+        public void ThenTheResultShouldBeYearsForTheFirstPerson(int expectedAge)
+        {
+            Assert.Equal(expectedAge, age);
+        }
+    }
+}
